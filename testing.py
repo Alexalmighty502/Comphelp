9bd72b89c83406b2bc3232a263e7184af7d73407
Alexalmighty502 <alexalmighty502@gmail.com>
2019-04-12 19:35:39 -0400

    more addiotions
    added a bit more of the program to be

--- testing.py
+++ testing.py
@@ -1,6 +1,5 @@
-
 itscrashy = {
-    "question": "?",
+    "question": "",
     "answers": {"yes": blank, "no": blank}
 }
 
@@ -13,12 +12,18 @@ boots = {
     "question": "Is you pc stable or does it crash?",
     "answers": {"yes": itscrashy, "no": isitslow}
 }
-
-noboot = {
-    "question": ":(",
-    "answers": ()
+checkpsu = {
+    "question": "Are you sure the Psu works?, This can be tested by using a paper clip to jump pin 3 and for on the clip side of the 24 pin",
+    "answers": {"yes": diditfix, "no": deadpsu}
 }
 
+noboot = {
+    "question": "Check to see if are any loose connections namely the 20-24 pin, cpu 6 pin and pcie power cords. Did this help?",
+    "answers": {"yes": diditfix, "no": checkpsu}
+}
+diditfix = {"question": "Did this fix your issue?",
+    "answers": {"yes": "Good to see the issue was fixed" exit(), "no": checkpsu}
+}
 doespcboot = {
     "question": "Does the pc boot?",
     "answers": {"yes": boots, "no": noboot}
@@ -31,4 +36,3 @@ while True:
         break
     answer = input()
     current_question = current_question["answers"][answer]
-
